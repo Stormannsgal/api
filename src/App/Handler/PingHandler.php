f5e386ec8111459c759336eff404487348a48991
BibaltiK <bibaltik@exdrals.de>
2023-10-18 00:10:11 +0200

    implements swaggerui and openapi attributes

--- src/App/Handler/PingHandler.php
+++ src/App/Handler/PingHandler.php
@@ -8,11 +8,34 @@ use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use OpenApi\Attributes as OA;
+use Fig\Http\Message\StatusCodeInterface as HTTP;
 
 use function time;
 
 class PingHandler implements RequestHandlerInterface
 {
+    #[OA\Get(
+        path: '/api/ping',
+        tags: ['System Information'],
+        responses: [
+            new OA\Response(
+                response: HTTP::STATUS_OK,
+                description: 'Success',
+                content: [
+                    new OA\JsonContent(
+                        properties: [
+                            new OA\Property(
+                                property: 'ack',
+                                description: 'actually time',
+                                type: 'string'
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )]
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         return new JsonResponse(['ack' => time()]);
