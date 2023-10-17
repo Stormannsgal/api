ddcf2accd106edb1cc751d8e4c30891d47cea50f
BibaltiK <bibaltik@exdrals.de>
2023-10-17 02:12:24 +0200

    moved index to api

--- public/api/index.php
+++ public/api/index.php
@@ -1,21 +1,21 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 // Delegate static file requests back to the PHP built-in webserver
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
+date_default_timezone_set('Europe/Berlin');
 
-chdir(dirname(__DIR__));
-require 'vendor/autoload.php';
+require_once __DIR__ . '/../../constants.php';
+
+require ROOT_DIR . 'vendor/autoload.php';
 
 /**
  * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
  */
 (function () {
     /** @var \Psr\Container\ContainerInterface $container */
-    $container = require 'config/container.php';
+    $container = require CONFIG_DIR .'container.php';
 
     /** @var \Mezzio\Application $app */
     $app = $container->get(\Mezzio\Application::class);
@@ -23,8 +23,8 @@ require 'vendor/autoload.php';
 
     // Execute programmatic/declarative middleware pipeline and routing
     // configuration statements
-    (require 'config/pipeline.php')($app, $factory, $container);
-    (require 'config/routes.php')($app, $factory, $container);
+    (require CONFIG_DIR . 'pipeline.php')($app, $factory, $container);
+    (require CONFIG_DIR .'routes.php')($app, $factory, $container);
 
     $app->run();
 })();
