e9d85960cd66d5857c4e7602248418a6cb4439a9
BibaltiK <bibaltik@exdrals.de>
2023-10-18 02:05:13 +0200

    add querybuilder with doctrine migrations

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -10,7 +10,7 @@ return [
         // Use 'aliases' to alias a service name to another service. The
         // key is the alias name, the value is the service to which it points.
         'aliases' => [
-            // Fully\Qualified\ClassOrInterfaceName::class => Fully\Qualified\ClassName::class,
+            PDO::class => 'database',
         ],
         // Use 'invokables' for constructor-less services, or services that do
         // not require arguments to the constructor. Map a service name to the
@@ -20,7 +20,8 @@ return [
         ],
         // Use 'factories' for services provided by callbacks/factory classes.
         'factories' => [
-            // Fully\Qualified\ClassName::class => Fully\Qualified\FactoryName::class,
+            'database' => App\Factory\DatabaseFactory::class,
+            'query' => App\Factory\QueryFactory::class,
         ],
     ],
 ];
