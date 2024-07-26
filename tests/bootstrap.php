f702907f1ca0472746685cc4454fcca32dfc9d9c
BibaltiK <bibaltik@exdrals.de>
2024-07-27 00:20:07 +0200

    fix bootstrap 4 functionaltest

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,4 @@
 putenv('APP_ENV=testing');
 system('rm ' . dirname(__FILE__) . '/../database/database.sqlite');
 system('php ' . dirname(__FILE__) . '/../bin/migrations.php migrations:sync-metadata-storage');
-system('php ' . dirname(__FILE__) . '/../bin/migrations.php migrations:migrate current+8 --no-interaction');
+system('php ' . dirname(__FILE__) . '/../bin/migrations.php migrations:migrate --no-interaction');
