778f15ffe16243d03dc562770e945aa1c3d34714
BibaltiK <bibaltik@exdrals.de>
2024-11-04 11:59:22 +0100

    test action with mariadb

---
+++ config/autoload/dependencies.testing.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+return [
+    'dependencies' => [
+        'aliases' => [
+            PDO::class => 'database',
+            Envms\FluentPDO\Query::class => 'query',
+            Ramsey\Uuid\Uuid::class => 'uuid',
+            Psr\Log\LoggerInterface::class => 'logger',
+        ],
+        'invokables' => [
+        ],
+        'factories' => [
+            'database' => Stormannsgal\Core\Factory\DatabaseFactory::class,
+            'query' => Stormannsgal\Core\Factory\QueryFactory::class,
+            'logger' => Stormannsgal\FunctionalTest\Mock\NullLoggerFactory::class
+        ],
+    ],
+];
