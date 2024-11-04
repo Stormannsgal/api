e858034c04932bdb188a8c29be0b0d160cfc2783
BibaltiK <bibaltik@exdrals.de>
2024-11-04 12:57:04 +0100

    try set correct env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 
-putenv('APP_ENV=testing');
+if (getenv('APP_ENV') !== 'action') {
+    putenv('APP_ENV=testing');
+}
 system('php ' . dirname(__FILE__) . '/../bin/migrations.php migrations:sync-metadata-storage');
 system('php ' . dirname(__FILE__) . '/../bin/migrations.php migrations:migrate --no-interaction');
 system('php ' . dirname(__FILE__) . '/../bin/migrations.php migrations:migrate Migrations\\\Version20231103224045_CreateAccountAccessAuthTable --no-interaction');
