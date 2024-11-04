4ea85eea959d8d7530d719d0e54b033e502fa4fe
BibaltiK <bibaltik@exdrals.de>
2024-11-04 12:35:44 +0100

    test action with mariadb

--- config/autoload/database.testing.php
+++ config/autoload/database.testing.php
@@ -3,7 +3,7 @@
 return [
     'database' => [
         'driver' => 'mysql',
-        'host' => 'localhost',
+        'host' => '127.0.0.1',
         'port' => '3306',
         'user' => 'dev',
         'password' => 'dev',
