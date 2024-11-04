030df8da1eff4d56e04ed9a33b29aad75f17450d
BibaltiK <bibaltik@exdrals.de>
2024-11-04 12:57:04 +0100

    try set correct env

--- config/autoload/database.action.php
+++ config/autoload/database.action.php
@@ -3,7 +3,7 @@
 return [
     'database' => [
         'driver' => 'mysql',
-        'host' => 'database-testing',
+        'host' => '127.0.0.1',
         'port' => '3306',
         'user' => 'dev',
         'password' => 'dev',
