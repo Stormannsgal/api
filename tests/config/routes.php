cff199fc0a9ddc4e83a2385cb8aff69e80135705
BibaltiK <bibaltik@exdrals.de>
2024-11-04 11:26:48 +0100

    change and activate functional tests

--- tests/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-use Mezzio\Application;
-use Stormannsgal\App\Handler\PingHandler;
-
-return static function (Application $app): void {
-    $app->get('/api/ping[/]', PingHandler::class, PingHandler::class);
-};
