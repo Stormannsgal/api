4bcaee134ea76f1b5ae91c0bef75836d04de2295
BibaltiK <bibaltik@exdrals.de>
2024-11-05 22:56:28 +0100

    Change so that an id is no longer required for inserting an account data record

--- tests/FunctionalTest/database/migrations/Version20990921210156_InsertAccountTestData.php
+++ tests/FunctionalTest/database/migrations/Version20990921210156_InsertAccountTestData.php
@@ -10,12 +10,12 @@ final class Version20990921210156_InsertAccountTestData extends AbstractMigratio
     public function up(Schema $schema): void
     {
         $sql = <<<SQL
-              INSERT INTO `Account` (`id`, `uuid`, `name`, `password`, `email`) VALUES
-                (1, '0192f76a139d7eb382df26219fb0fa2e', 'Owner', '$2y$10$1O6FG7z6ozavsNXWRcLdIuTyxH79y9iySKuiGNb9fGbjiFaa6K.wa', 'owner@example.com'),
-                (2, '0192f76b0b4e7a4bb4d872eb2f491066', 'Administrator', '$2y$10$9PfaGkyQYc8GK8a3nRSbSupxUTrX/yXsMNqWawZ7FAhTVBv3azSf.', 'admin@example.com'),
-                (3, '0192f76d06747f13b77d36d958ffd32d', 'Moderator', '$2y$10$79SWSxmeKTswZS2xvkJ/reQ10QWfyYAvSb/J9L0S4XxAmYUhlVIo2', 'moderator@example.com'),
-                (4, '0192f770b91d7b57893d170d52ebfdd1', 'User', '$2y$10$4mhYlAiwxUe84dqnEMmACOxek12Hjn5NlyF1DBiH1ginLjSCn.fPu', 'user@example.com'),
-                (5, '0192cfe9233172cd972f837375c8f53e', 'Account Name', '$2y$10$4dye9Kn.UQkHDalnu9FS2uGwPgCZfrB0qYYJxUIcD8SdUQi8xDtp6', 'valid@example.com');
+              INSERT INTO `Account` (`uuid`, `name`, `password`, `email`) VALUES
+                ('0192f76a139d7eb382df26219fb0fa2e', 'Owner', '$2y$10$1O6FG7z6ozavsNXWRcLdIuTyxH79y9iySKuiGNb9fGbjiFaa6K.wa', 'owner@example.com'),
+                ('0192f76b0b4e7a4bb4d872eb2f491066', 'Administrator', '$2y$10$9PfaGkyQYc8GK8a3nRSbSupxUTrX/yXsMNqWawZ7FAhTVBv3azSf.', 'admin@example.com'),
+                ('0192f76d06747f13b77d36d958ffd32d', 'Moderator', '$2y$10$79SWSxmeKTswZS2xvkJ/reQ10QWfyYAvSb/J9L0S4XxAmYUhlVIo2', 'moderator@example.com'),
+                ('0192f770b91d7b57893d170d52ebfdd1', 'User', '$2y$10$4mhYlAiwxUe84dqnEMmACOxek12Hjn5NlyF1DBiH1ginLjSCn.fPu', 'user@example.com'),
+                ('0192cfe9233172cd972f837375c8f53e', 'Account Name', '$2y$10$4dye9Kn.UQkHDalnu9FS2uGwPgCZfrB0qYYJxUIcD8SdUQi8xDtp6', 'valid@example.com');
         SQL;
 
         $this->addSql($sql);
