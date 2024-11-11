252ae0dc2133396cc474dc4c120f2c0b458d3f28
BibaltiK <bibaltik@exdrals.de>
2024-11-11 20:15:45 +0100

    change 4 cors

--- config/autoload/cors.develope.php
+++ config/autoload/cors.develope.php
@@ -1,12 +1,11 @@
 <?php declare(strict_types=1);
 
-
 use Mezzio\Cors\Configuration\ConfigurationInterface;
 
 return [
     ConfigurationInterface::CONFIGURATION_IDENTIFIER => [
-        'allowed_origins' => ['*//localhost:*',],
-        'allowed_headers' => ['x-ident', 'Authorization', 'Authentication'], // No custom headers allowed
+        'allowed_origins' => ['http://localhost:5173','http://localhost'],
+        'allowed_headers' => ['x-ident', 'Authorization', 'Authentication', 'Content-Type'], // No custom headers allowed
         'allowed_max_age' => '3600', // 60 minutes
         'credentials_allowed' => true, // Disallow cookies
         'exposed_headers' => [], // No headers are exposed
